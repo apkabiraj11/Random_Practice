cda72b649a5385bccddab8f643a7483f977427b0
Apurba Chandra Kabiraj <apkabiraj11@gmail.com>
2026-04-18 23:20:33 +0600

    Updated 18 april, 2026

---
+++ 15_april_2026/Smilo and Monsters cf.cpp
@@ -0,0 +1,92 @@
+ #include <bits/stdc++.h>
+ // #include <ext/pb_ds/assoc_container.hpp>
+ // #include <ext/pb_ds/tree_policy.hpp>
+ using namespace std;
+ // using namespace __gnu_pbds;
+ 
+ #define ll long long int
+ #define ln '\n'
+ #define yes cout << "YES" << ln
+ #define no cout << "NO" << ln
+ const int N = 1e6 + 9, mod = 1e9 + 7;
+ 
+ // template <typename T>
+ // using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ // Functions:
+ // 1. find_by_order(k) -> returns iterator to the k-th element (0-indexed)
+ // 2. order_of_key(x) -> returns number of elements strictly smaller than x
+ 
+ 
+ void solve() {
+     int n;
+     cin >> n;
+     multiset<int>st;
+     for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        st.insert(x);
+     }
+     ll ans = 0;
+     ll x = 0;
+     while(!st.empty()){
+        int big = *st.rbegin();
+        st.erase(prev(st.end()));
+        ll sum = 0;
+
+        while(!st.empty()){
+          if(sum + *st.begin() < big){
+            sum += *st.begin();
+            st.erase(st.begin());
+          }
+          else break;
+        }
+        if(!st.empty()){
+            int front = *st.begin();
+            st.erase(st.begin());
+            front -= (big - sum);
+            sum = big;
+            if(front > 0)
+                st.insert(front);
+        }
+
+        sum += big;
+
+        if(sum >= 2){
+          ans += sum / 2;
+          if(sum % 2) {
+            st.insert(1);
+          }
+          ans++;
+        }
+        else {
+          ans++;
+        }
+
+     }
+     cout << ans << ln;
+ }
+ 
+ int main() {
+     ios::sync_with_stdio(false);
+     cin.tie(nullptr);
+ 
+     int t = 1;
+     cin >> t;
+     // int casee = 1;
+     while(t--) {
+         // cout << "Case " << casee << ": ";
+         solve();
+         // casee++;
+     }
+ 
+     return 0;
+ }
+ 
+ /**************
+ 
+ LEARNING IS FUN
+ SO ENJOY IT 
+ 
+ ***************/
+ 
+ 
